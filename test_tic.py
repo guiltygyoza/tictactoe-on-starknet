682a45f485e429f0ba5b522996a205b74b1f7a42
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2021-10-09 15:35:09 -0400

    adjusted s domain to allow optimization to be equivalent to pre-optimized logic

--- test_tic.py
+++ test_tic.py
@@ -1,5 +1,6 @@
 import pytest
 from starkware.starknet.testing.starknet import Starknet
+from timeit import default_timer as timer
 
 @pytest.mark.asyncio
 async def test_tic():
@@ -12,11 +13,28 @@ async def test_tic():
 
     x=2
     y=2
-    ret = await contract.user_move(x=x, y=y).invoke()
-    print(f"> user_move(x={x}, y={y}) returns: {ret}")
 
-    #for y in range(3):
-    #    for x in range(3):
-    #        ret = await contract.view_board(x=x, y=y).call()
-    #        print(f'{ret.z}', end=' ')
-    #    print()
+    time1 = timer()
+
+    ret_user_move = await contract.user_move(x=x, y=y).invoke()
+
+    time2 = timer()
+
+    z_s = {}
+    for y in range(3):
+        for x in range(3):
+            ret = await contract.view_board(x=x, y=y).call()
+            z_s[(x,y)] = ret.z
+            #print(f'{ret.z}', end=' ')
+        #print()
+
+    time3 = timer()
+
+    print(f"> user_move(x={x}, y={y}) returns: {ret_user_move}")
+    for y in range(3):
+        for x in range(3):
+            print(f'{z_s[(x,y)]}', end=' ')
+        print()
+
+    print(f"> time2-time1: {time2 - time1} sec")
+    print(f"> time3-time2: {time3 - time2} sec")
