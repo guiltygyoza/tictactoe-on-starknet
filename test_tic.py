27502192942e1efa850e05877142eb7018eaba02
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2021-10-08 23:44:45 -0400

    trimmed q table to minimize footprint and speed up compilation; eyeball test results match expected results from python script in terms of AI's countermove to human player's move

--- test_tic.py
+++ test_tic.py
@@ -6,9 +6,17 @@ async def test_tic():
     starknet = await Starknet.empty()
     contract = await starknet.deploy("tic.cairo")
 
-    ret = await contract.toy_lookup(
-        x=10, y=100).call()
-    print(f"> toy_lookup(x=10, y=100) returns: {ret}")
+    #ret = await contract.toy_lookup(
+    #    x=10, y=100).call()
+    #print(f"> toy_lookup(x=10, y=100) returns: {ret}")
 
-    ret = await contract.user_move(x=1, y=1).invoke()
-    print(f"> user_move(x=1, y=1) returns: {ret}")
+    x=2
+    y=2
+    ret = await contract.user_move(x=x, y=y).invoke()
+    print(f"> user_move(x={x}, y={y}) returns: {ret}")
+
+    for y in range(3):
+        for x in range(3):
+            ret = await contract.view_board(x=x, y=y).call()
+            print(f'{ret.z}', end=' ')
+        print()
