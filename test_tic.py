ad8941da5f8074cade210656eebd0c7f91881ac9
guiltygyoza <59590480+guiltygyoza@users.noreply.github.com>
2021-10-09 22:14:33 -0400

    reverted back to uncompressed flattened lookup code to explore other ways of making cairo compiler happy

--- test_tic.py
+++ test_tic.py
@@ -7,31 +7,9 @@ async def test_tic():
     starknet = await Starknet.empty()
     contract = await starknet.deploy("tic.cairo")
 
-    #ret = await contract.toy_lookup(
-    #    x=10, y=100).call()
-    #print(f"> toy_lookup(x=10, y=100) returns: {ret}")
-
-    x=2
-    y=2
-
-    time1 = timer()
-
-    ret_user_move = await contract.user_move(x=x, y=y).invoke()
-
-    time2 = timer()
-
-    board = [0,0,0,0,0,0,0,0,0]
-    (
-        board[0], board[1], board[2],
-        board[3], board[4], board[5],
-        board[6], board[7], board[8]
-    ) = await contract.view_board().call()
-
-    time3 = timer()
-
-    await contract.reset_board().invoke()
-
-    time4 = timer()
+    # board initialization
+    await contract.test_configure_board(1,1,2).invoke() # user is 2
+    await contract.test_configure_board(2,2,1).invoke() # AI is 1
 
     board_0 = [0,0,0,0,0,0,0,0,0]
     (
@@ -40,22 +18,42 @@ async def test_tic():
         board_0[6], board_0[7], board_0[8]
     ) = await contract.view_board().call()
 
-    print(f"> user_move(x={x}, y={y}) returns: {ret_user_move}")
-
-    print("board after user_move and AI's countermove:")
-    for y in range(3):
-        for x in range(3):
-            print(board[3*y + x], end=' ')
-        print()
-
-    print()
-
-    print("board after reset_board():")
+    print("> board after initial configuration:")
     for y in range(3):
         for x in range(3):
             print(board_0[3*y + x], end=' ')
         print()
+    print()
 
-    print(f"> time2-time1: {time2 - time1} sec")
-    print(f"> time3-time2: {time3 - time2} sec")
-    print(f"> time4-time3: {time4 - time3} sec")
+    # user_move
+    x=2
+    y=0
+
+    time1 = timer()
+    ret_user_move = await contract.user_move(x=x, y=y).invoke()
+    time2 = timer()
+
+    board = [0,0,0,0,0,0,0,0,0]
+    (
+        board[0], board[1], board[2],
+        board[3], board[4], board[5],
+        board[6], board[7], board[8]
+    ) = await contract.view_board().call()
+    time3 = timer()
+
+    await contract.reset_board().invoke()
+    time4 = timer()
+
+    # print results
+    print(f"> user_move(x={x}, y={y}) returns: {ret_user_move}")
+
+    print("> board after user_move and AI's countermove:")
+    for y in range(3):
+        for x in range(3):
+            print(board[3*y + x], end=' ')
+        print()
+    print()
+
+    print(f"> time2-time1: {time2 - time1} sec (user_move)")
+    print(f"> time3-time2: {time3 - time2} sec (view_board)")
+    print(f"> time4-time3: {time4 - time3} sec (reset_board)")
